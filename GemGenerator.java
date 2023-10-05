58bb888fa86ed5bf55842d63a950b694b66986fa
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2023-10-05 04:28:32 +0300

    first commit

---
+++ GemGenerator.java
@@ -0,0 +1,9 @@
+public class GemGenerator extends ItemFactory {
+    public class GemGenerator extends ItemFactory {
+        @Override
+        public GemItem createItem() {
+            GemReward gemReward = new GemReward();
+            gemReward.open();
+            return gemReward;
+    
+}
